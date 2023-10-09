827c2ef6478c05b251fa347a73553c7dfed85907
erastov-alex <erastov.alex@gmail.com>
2023-10-09 18:02:21 +0300

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "SIIM-ACR Pneumothorax Segmentation 2019"
 PROJECT_NAME_FULL: str = "SIIM-ACR Pneumothorax Segmentation 2019"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
